2ba7d417b2ad44d928412a57bebfacf070d63e4a
TechSupport84 <trainingtech84@gmail.com>
2025-01-24 20:28:17 +0300

    added

--- src/screens/Api_Url.jsx
+++ src/screens/Api_Url.jsx
@@ -1,3 +1,2 @@
-export  const API_URL ="http://localhost:5000/api"
-export  const API_URL_image ="http://localhost:5000"
-//https://jeancy24sur-b8232b5db48b.herokuapp.com/api
+export  const API_URL ="https://jeancy24sur-280f233049ed.herokuapp.com/api"
+export  const API_URL_image ="https://jeancy24sur-280f233049ed.herokuapp.com"
