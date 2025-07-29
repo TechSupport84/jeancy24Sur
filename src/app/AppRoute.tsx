122f5ad9b95c1188530b0187cd1d12d1e13f84ca
jeancy001 <jeancympoy24@gmail.com>
2025-07-29 15:32:12 +0100

    added

--- src/app/AppRoute.tsx
+++ src/app/AppRoute.tsx
@@ -3,7 +3,7 @@ import HomePage from "../pages/HomePage";
 
 import NavBar from "../components/NavBar";
 import LearnEnglish from "../pages/LearnEnglish";
-import LearnPython from "../sreens/learnPython";
+import LearnPython from "../pages/LearnPython";
 
 
 function AppRoute() {
